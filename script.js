ae9678713a6375f4268b8fce631eaae8861ab2d0
jairo <jairocoppis@outlook.com>
2022-09-11 14:43:41 -0300

    Agregando codigo al proyecto final

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 
+let noticias = 
+
+[{imgURL: "images/energia.svg" , informacion: " CELDA informa un CORTE PROGRAMADO DE ENERGÍA ELÉCTRICA en el Sector Quintas al oeste de la localidad, específicamente usuarios desde calle Vieytes hacia el Aerogenerador y hasta el cementerio local, sin afectar al Parque Industrial. El mismo será mañana miércoles 12 del corriente, de 9:00 a 13:00hs aprox. motivo es por trabajos sobre líneas del sector."},
+
+{imgURL: "images/internet.svg", informacion: "CELDA informa un CORTE PROGRAMADO DE INTERNET en el Sector Quintas al oeste de la localidad, específicamente usuarios desde calle Vieytes hacia el Aerogenerador y hasta el cementerio local, sin afectar al Parque Industrial. El mismo será mañana miércoles 12 del corriente, de 9:00 a 13:00hs aprox. motivo es por trabajos sobre líneas del sector."},
+
+{imgURL: "images/agua.svg", informacion: "CELDA informa un CORTE PROGRAMADO DE AGUA en el Sector Quintas al oeste de la localidad, específicamente usuarios desde calle Vieytes hacia el Aerogenerador y hasta el cementerio local, sin afectar al Parque Industrial. El mismo será mañana miércoles 12 del corriente, de 9:00 a 13:00hs aprox. motivo es por trabajos sobre líneas del sector." }
+]
+
+
+let noticiasJSON = JSON.stringify(noticias)
+localStorage.setItem("noticias", noticiasJSON)
+
+
+let noticiasJS = JSON.parse(localStorage.getItem("noticias"))
+console.log(noticiasJS)
+
+
+//-----------------------------------------------------------------------------
+
+
 let contenedorAlertas = document.getElementById("noticias")
 
 let botonAlert = document.getElementById("botonAlert")
@@ -20,32 +41,56 @@ function mostrarAlerta () {
   })
 }
 
-function elemento() {
-
-fetch('./data.json')
-.then((respuesta) => respuesta.json())
-.then((data) => { console.log(data.noticias)
-  for (const noticia of data.noticias) {
-    let tarjetaNoticia = document.createElement("div")
-    tarjetaNoticia.className = "noticia"
-    tarjetaNoticia.innerHTML = `
+function elemento() { 
   
-      <section class="container mt-4" id="Alertas">
-      <div class="row">
-      <div class="d-flex justify-content-center flex-wrap">
-      <div class="col-sm-12 col-lg-4 col-md-12 tarjeta-item tarjetalogo">
-      <img src=${noticia.imgURL} class="img-fluid" mt-2>
-      <p>${noticia.informacion}</p>
-  
-    `
-    contenedorAlertas.append(tarjetaNoticia);
-  }
-  }
+for (const noticia of noticiasJS) {
+  let tarjetaNoticia = document.createElement("div")
+  tarjetaNoticia.className = "noticia"
+  tarjetaNoticia.innerHTML = `
 
+    <section class="container mt-4" id="Alertas">
+    <div class="row">
+    <div class="d-flex justify-content-center flex-wrap">
+    <div class="col-sm-12 col-lg-4 col-md-12 tarjeta-item tarjetalogo">
+    <img src=${noticia.imgURL} class="img-fluid" mt-2>
+    <p>${noticia.informacion}</p>
 
-)
+  `
+  contenedorAlertas.append(tarjetaNoticia);
+}
 }
 
 
 
+//-----------------------------------------------------------------------------------------------------------
 
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn1");
+
+
+  btn.addEventListener("click", function () {
+    
+
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+
+    question.classList.toggle("show-text");
+  });
+});
+
+
+// const btns = document.querySelectorAll(".question-btn1");
+
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     const question = e.currentTarget.parentElement.parentElement;
+
+//     question.classList.toggle("show-text");
+//   });
+// });
